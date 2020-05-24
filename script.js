8a1efb607e54651760ad3326a9b12c0be51df204
johnyu763 <johnyu763@gmail.com>
2020-05-23 17:39:57 -0700

    more dropdown

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-function myFunction() {
-  document.getElementById("myProjDrop").classList.toggle("show");
+function getProjDrop() {
+    var dropdowns = document.getElementsByClassName("aboutDrop");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+    document.getElementById("myProjDrop").classList.toggle("show");
+    
+}
+
+function getAboutDrop(){
+    var dropdowns = document.getElementsByClassName("projDrop");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+    document.getElementById("myAboutDrop").classList.toggle("show");
 }
 
 //closes dropdown if not selected
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("projDrop");
+    var dropdowns = document.getElementsByClassName("dropdown");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
